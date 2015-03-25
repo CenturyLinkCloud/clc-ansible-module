0334e60717866295752559dc856ef1a101616332
bjfelton <bjfelton@gmail.com>
2015-03-25 10:41:26 -0500

    Re-refactored clc_group so that it works.  Tests to follow.

--- clc_group.py
+++ clc_group.py
@@ -19,22 +19,56 @@ else:
     clc_found = True
 
 class ClcGroup():
-    clc = clc_sdk
-    module = None
-    group_dict = {}
 
     def __init__(self, module):
-
+        self.clc = clc_sdk
         self.module = module
+        self.group_dict = {}
+
+
+    def do_work(self):
+        p = self.module.params
 
         if not clc_found:
             self.module.fail_json(msg='clc-python-sdk required for this module')
+
+        self._clc_set_credentials()
+        self.group_dict = self._get_group_tree_for_datacenter(datacenter=p['location'])
+
+        if p['state'] == "absent":
+            changed, group = self._ensure_group_is_absent(p)
+        else:
+            changed, group = self._ensure_group_is_present(p)
+
+        if group:
+            group = group.data
+
+        self.module.exit_json(changed=changed, group=group)
+
+    #
+    #  Functions to define the Ansible module and its arguments
+    #
+
+    @staticmethod
+    def define_argument_spec():
+        argument_spec = dict(
+            name=dict(required=True),
+            description=dict(default=None),
+            parent=dict(default=None),
+            location=dict(default=None),
+            alias=dict(default=None),
+            custom_fields=dict(type='list', default=[]),
+            server_ids=dict(type='list', default=[]),
+            state=dict(default='present', choices=['present', 'absent'])
+            )
+
+        return argument_spec
 
     #
     #   Module Behavior Functions
     #
 
-    def set_clc_credentials_from_env(self):
+    def _clc_set_credentials(self):
             e = os.environ
 
             v2_api_passwd = None
@@ -49,35 +83,22 @@ class ClcGroup():
             self.clc.v2.SetCredentials(api_username=v2_api_username, api_passwd=v2_api_passwd)
 
 
-    def update_groups(self):
-        p = self.module.params
-        #TODO:  Make a validation function that sets this if parent==None
-        if not p['parent']:
-            p['parent'] = self.clc.v2.Datacenter(location=p['location']).RootGroup().data['name']
-
-        self.group_dict = self._get_group_tree_for_datacenter(datacenter=p['location'])
-
-
-    def ensure_group_is_absent(self):
+    def _ensure_group_is_absent(self, p):
         changed = False
         group = None
-        p = self.module.params
 
         if self._group_exists(group_name=p['name'], parent_name=p['parent']):
             self._delete_group(p)
             changed = True
         return changed, group
 
-
     def _delete_group(self, p):
         group, parent = self.group_dict[p['name']]
         group.Delete()
 
-
-    def ensure_group_is_present(self):
+    def _ensure_group_is_present(self, p):
         changed = False
         group = None
-        p = self.module.params
 
         parent_exists = self._group_exists(group_name=p['parent'], parent_name=None)
         child_exists = self._group_exists(group_name=p['name'], parent_name=p['parent'])
@@ -116,10 +137,20 @@ class ClcGroup():
         return result
 
 
+
+    def _group_exists(self, group_name, parent_name):
+        result = False
+        if group_name in self.group_dict:
+            group, parent = self.group_dict[group_name]
+
+            if parent_name == parent.name or not parent_name:
+                result = True
+
+        return result
+
     def _get_group_tree_for_datacenter(self, datacenter=None, alias=None):
         root_group = self.clc.v2.Datacenter(location=datacenter).RootGroup()
         return self._walk_group_tree(parent_group=None, child_group=root_group)
-
 
     def _walk_group_tree(self, parent_group, child_group):
         result = {str(child_group): (child_group, parent_group)}
@@ -130,59 +161,21 @@ class ClcGroup():
         return result
 
 
-    def _get_group(self, group_name, datacenter=None, alias=None):
+    def _get_group(self, module, group_name, datacenter=None, alias=None):
         result = None
         try:
             result = self.clc.v2.Datacenter(location=datacenter, alias=alias).Groups().Get(group_name)
         except CLCException, e:
             if "Group not found" not in e.message:
-                self.module.fail_json(msg='error looking up group: %s' % e)
+                module.fail_json(msg='error looking up group: %s' % e)
         return result
 
-#
-#  Functions to define the Ansible module and its arguments
-#
-
-def create_ansible_module():
-    argument_spec = define_argument_spec()
-
-    result = AnsibleModule(
-        argument_spec=argument_spec
-    )
-    return result
-
-
-@staticmethod
-def define_argument_spec():
-    argument_spec = dict(
-        name=dict(required=True),
-        description=dict(default=None),
-        parent=dict(default=None),
-        location=dict(default=None),
-        alias=dict(default=None),
-        custom_fields=dict(type='list', default=[]),
-        server_ids=dict(type='list', default=[]),
-        state=dict(default='present', choices=['present', 'absent'])
-        )
-
-    return argument_spec
-
-
 def main():
-    module = create_ansible_module()
-    p = module.params
+    module = AnsibleModule(
+            argument_spec=ClcGroup.define_argument_spec()
+        )
     clc_group = ClcGroup(module)
-    clc_group.set_clc_credentials_from_env()
-
-    if p['state'] == "absent":
-        changed, group = clc_group.ensure_group_is_absent(p)
-    else:
-        changed, group = clc_group.ensure_group_is_present(p)
-
-    if group:
-        group = group.data
-
-    module.exit_json(changed=changed, group=group)
+    clc_group.do_work()
 
 
 if __name__ == '__main__':
