b300ce50d50599a0ec73a945e5f1085c57dfbb05
jhinds <jhinds2010@gmail.com>
2015-06-16 08:53:46 -0500

    added documentation and checkmode for publicip module

--- src/main/python/clc_ansible_module/clc_publicip.py
+++ src/main/python/clc_ansible_module/clc_publicip.py
@@ -1,5 +1,74 @@
 #!/usr/bin/python
+DOCUMENTATION = '''
+module: clc_public_ip
+short_description: add and/or delete public ips on servers in CenturyLink Cloud.
+description:
+  - An Ansible module to add or delete public ip addresses on an existing server or servers in CenturyLink Cloud.
+options:
+  protocol:
+    descirption:
+      - The protocol that the public IP will listen for.
+    default: TCP
+    required: False
+  ports:
+    description:
+      - A list of ports to expose.
+    required: True
+  server_ids:
+    description:
+      - A list of servers to create publci ips on.
+    required: True
+  state:
+    description:
+      - Determine wheteher to create or delete public IPs. If present module will not create a second public ip if one
+        already exists.
+    default: present
+    choices: ['present', 'absent']
+    required: False
+  wait:
+    description:
+      - Whether to wait for the tasks to finish before returning.
+    choices: [ True, False ]
+    default: True
+    required: False
+'''
 
+EXAMPLES = '''
+- name: Add Public IP to Server
+  hosts: localhost
+  gather_facts: False
+  connection: local
+  tasks:
+    - name: Create Public IP For Servers
+      clc_public_ip:
+        protocol: 'TCP'
+        ports:
+            - 80
+        server_ids:
+            - UC1ACCTSRVR01
+            - UC1ACCTSRVR02
+        state: present
+      register: clc
+
+    - name: debug
+      debug: var=clc
+
+- name: Delete Public IP from Server
+  hosts: localhost
+  gather_facts: False
+  connection: local
+  tasks:
+    - name: Create Public IP For Servers
+      clc_public_ip:
+        server_ids:
+            - UC1ACCTSRVR01
+            - UC1ACCTSRVR02
+        state: absent
+      register: clc
+
+    - name: debug
+      debug: var=clc
+'''
 import socket
 import time
 from ansible.module_utils.basic import *  # pylint: disable=W0614
@@ -28,17 +97,26 @@ class ClcPublicIp(object):
     SOCKET_CONNECTION_TIMEOUT = 3
 
     def __init__(self, module):
+        """
+        Construct module
+        """
         self.module = module
         if not CLC_FOUND:
             self.module.fail_json(
                 msg='clc-python-sdk required for this module')
 
     def process_request(self, params):
+        """
+        Process the request - Main Code Path
+        :param params: dictionary of module parameters
+        :return: Returns with either an exit_json or fail_json
+        """
         self.set_clc_credentials_from_env()
         server_ids = params['server_ids']
         ports = params['ports']
         protocol = params['protocol']
         state = params['state']
+        wait = params['wait']
         command_list = []
 
         if state == 'present':
@@ -62,6 +140,14 @@ class ClcPublicIp(object):
             server_ids=result_server_ids)
 
     def run_clc_commands(self, command_list):
+        """
+        Executes the CLC commands
+        :param command_list: list of commands to run
+        :return: (has_made_changes, result_changed_servers, changed_server_ids)
+            has_made_changes: Boolean on whether a change was made
+            result_changed_servers: The result from the CLC API call
+            changed_server_ids: A list of the changed servers
+        """
         requests_list = []
         changed_servers = []
         for command in command_list:
@@ -77,47 +163,74 @@ class ClcPublicIp(object):
         return has_made_changes, result_changed_servers, changed_server_ids
 
     def ip_create_command(self, server_ids, protocol, ports):
+        """
+        Creates public ip
+        :param server_ids: list of server ids to change
+        :param protocol:  The protocol the public IP will listen for
+        :param ports: List of ports to expose
+        :return: returns servers that IPs were created on
+        """
         servers = self._get_servers_from_clc_api(
             server_ids,
             'Failed to obtain server list from the CLC API')
-        servers_to_change = [
-            server for server in servers if len(
-                server.PublicIPs().public_ips) == 0]
-        ports_to_expose = [{'protocol': protocol, 'port': port}
-                           for port in ports]
-        ClcPublicIp._push_metric(ClcPublicIp.STATS_PUBLICIP_CREATE,len(servers_to_change))
+        if not self.module.check_mode:
+            servers_to_change = [
+                server for server in servers if len(
+                    server.PublicIPs().public_ips) == 0]
+            ports_to_expose = [{'protocol': protocol, 'port': port}
+                               for port in ports]
+            ClcPublicIp._push_metric(ClcPublicIp.STATS_PUBLICIP_CREATE,len(servers_to_change))
         return [server.PublicIPs().Add(ports_to_expose)
                 for server in servers_to_change], servers_to_change
 
     def ip_delete_command(self, server_ids):
+        """
+        Deletes public ip
+        :param server_ids: list of servers to delete public ips from
+        :return: returns servers that IPs were deleted from
+        """
         servers = self._get_servers_from_clc_api(
             server_ids,
             'Failed to obtain server list from the CLC API')
-        servers_to_change = [
-            server for server in servers if len(
-                server.PublicIPs().public_ips) > 0]
+        if not self.module.check_mode:
+            servers_to_change = [
+                server for server in servers if len(
+                    server.PublicIPs().public_ips) > 0]
 
-        ips_to_delete = []
-        for server in servers_to_change:
-            for ip_address in server.PublicIPs().public_ips:
-                ips_to_delete.append(ip_address)
-        ClcPublicIp._push_metric(ClcPublicIp.STATS_PUBLICIP_DELETE,len(servers_to_change))
+            ips_to_delete = []
+            for server in servers_to_change:
+                for ip_address in server.PublicIPs().public_ips:
+                    ips_to_delete.append(ip_address)
+            ClcPublicIp._push_metric(ClcPublicIp.STATS_PUBLICIP_DELETE,len(servers_to_change))
         return [ip.Delete() for ip in ips_to_delete], servers_to_change
 
     def _wait_for_requests_to_complete(self, requests_lst, action='create'):
+        """
+        Waits for the requests to complete
+        :param requests_lst: a list of the requests that a being waited on
+        :param action: action that is waiting to complete
+        :return: none
+        """
         for request in requests_lst:
             request.WaitUntilComplete()
             for request_details in request.requests:
                 if request_details.Status() != 'succeeded':
                     self.module.fail_json(
                         msg='Unable to ' +
-                        action +
-                        ' Public IP for ' +
-                        request.server.id +
-                        ': ' +
-                        request.Status())
+                            action +
+                            ' Public IP for ' +
+                            request.server.id +
+                            ': ' +
+                            request.Status())
 
     def _refresh_server_public_ips(self, servers_to_refresh):
+        """
+        Refresh the public ip server list
+        :param servers_to_refresh: list of servers to refresh
+        :return: (refreshed_server_ids, refreshed_servers)
+            refreshed_server_ids: whether the ids were refreshed or not
+            refreshed_servers: list of servers that were refreshed
+        """
         refreshed_server_ids = [server.id for server in servers_to_refresh]
         refreshed_servers = self._get_servers_from_clc_api(
             refreshed_server_ids,
@@ -126,6 +239,13 @@ class ClcPublicIp(object):
 
     @staticmethod
     def _parse_server_results(servers):
+        """
+        Parses servers results for IP information
+        :param servers: list of servers
+        :return: (changed, servers_result)
+            changed: returns a Boolean if public Ip was added
+            servers_result: returns ip data of the servers
+        """
         servers_result = []
         changed = False
         for server in servers:
@@ -142,6 +262,9 @@ class ClcPublicIp(object):
         return changed, servers_result
 
     def _get_servers_from_clc_api(self, server_ids, message):
+        """
+        Gets list of servers form CLC api
+        """
         try:
             return self.clc.v2.Servers(server_ids).servers
         except CLCException as exception:
@@ -153,11 +276,15 @@ class ClcPublicIp(object):
             server_ids=dict(type='list', required=True),
             protocol=dict(default='TCP'),
             ports=dict(type='list'),
+            wait=dict(default=True),
             state=dict(default='present', choices=['present', 'absent']),
         )
         return argument_spec
 
     def set_clc_credentials_from_env(self):
+        """
+        Sets the CLC environment variables
+        """
         env = os.environ
         v2_api_token = env.get('CLC_V2_API_TOKEN', False)
         v2_api_username = env.get('CLC_V2_API_USERNAME', False)
@@ -179,6 +306,9 @@ class ClcPublicIp(object):
 
     @staticmethod
     def _push_metric(path, count):
+        """
+        Pushes out metrics of when module is run
+        """
         try:
             sock = socket.socket()
             sock.settimeout(ClcPublicIp.SOCKET_CONNECTION_TIMEOUT)
@@ -194,8 +324,13 @@ class ClcPublicIp(object):
 
 
 def main():
+    """
+    The main function.  Instantiates the module and calls process_request.
+    :return: none
+    """
     module = AnsibleModule(
-        argument_spec=ClcPublicIp.define_argument_spec()
+        argument_spec=ClcPublicIp.define_argument_spec(),
+        supports_check_mode=True
     )
     clc_public_ip = ClcPublicIp(module)
     clc_public_ip.process_request(module.params)
