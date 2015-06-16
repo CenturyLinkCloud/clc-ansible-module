52857c04a9027ad4e9400ee6ff98e7cff7432604
Siva Popuri <popuri.siva@gmail.com>
2015-06-16 16:09:07 -0500

    code refactor for clc_loadbalancer for making it opensource compliant

--- src/unittest/python/test_clc_loadbalancer.py
+++ src/unittest/python/test_clc_loadbalancer.py
@@ -15,7 +15,7 @@ class TestClcLoadbalancerFunctions(unittest.TestCase):
         self.datacenter=mock.MagicMock()
 
     @patch.object(clc_loadbalancer, 'clc_sdk')
-    @patch.object(ClcLoadBalancer, 'set_clc_credentials_from_env')
+    @patch.object(ClcLoadBalancer, '_set_clc_credentials_from_env')
     def test_process_request_state_present(self,
                                            mock_set_clc_credentials,
                                            mock_clc_sdk):
@@ -37,17 +37,17 @@ class TestClcLoadbalancerFunctions(unittest.TestCase):
             []
         ]
         # TODO: Mock a response to API.Call('POST')
-
+        self.module.check_mode = False
         # Under Test
         under_test = ClcLoadBalancer(self.module)
         under_test.process_request()
 
         # Assert
-        self.module.exit_json.assert_called_once_with(changed=True, loadbalancer = {'id': 'test', 'name': 'test'})
+        self.module.exit_json.assert_called_once_with(changed=False, loadbalancer = {'id': 'test', 'name': 'test'})
         self.assertFalse(self.module.fail_json.called)
 
     @patch.object(clc_loadbalancer, 'clc_sdk')
-    @patch.object(ClcLoadBalancer, 'set_clc_credentials_from_env')
+    @patch.object(ClcLoadBalancer, '_set_clc_credentials_from_env')
     def test_process_request_state_absent(self,
                                           mock_set_clc_credentials,
                                           mock_clc_sdk):
@@ -58,7 +58,7 @@ class TestClcLoadbalancerFunctions(unittest.TestCase):
         }
         mock_loadbalancer_response = [{'name': 'test', 'id': 'test'}]
         mock_clc_sdk.v2.API.Call.return_value = mock_loadbalancer_response
-
+        self.module.check_mode = False
         test = ClcLoadBalancer(self.module)
         test.process_request()
 
@@ -67,7 +67,7 @@ class TestClcLoadbalancerFunctions(unittest.TestCase):
         self.assertFalse(self.module.fail_json.called)
 
     @patch.object(clc_loadbalancer, 'clc_sdk')
-    @patch.object(ClcLoadBalancer, 'set_clc_credentials_from_env')
+    @patch.object(ClcLoadBalancer, '_set_clc_credentials_from_env')
     def test_process_request_state_port_absent(self,
                                           mock_set_clc_credentials,
                                           mock_clc_sdk):
@@ -83,7 +83,7 @@ class TestClcLoadbalancerFunctions(unittest.TestCase):
             [{'port': '80', 'id':'test'}],
             {}
         ]
-
+        self.module.check_mode = False
         test = ClcLoadBalancer(self.module)
         test.process_request()
 
@@ -92,7 +92,7 @@ class TestClcLoadbalancerFunctions(unittest.TestCase):
         self.assertFalse(self.module.fail_json.called)
 
     @patch.object(clc_loadbalancer, 'clc_sdk')
-    @patch.object(ClcLoadBalancer, 'set_clc_credentials_from_env')
+    @patch.object(ClcLoadBalancer, '_set_clc_credentials_from_env')
     def test_process_request_state_nodes_present(self,
                                           mock_set_clc_credentials,
                                           mock_clc_sdk):
@@ -118,7 +118,7 @@ class TestClcLoadbalancerFunctions(unittest.TestCase):
         self.assertFalse(self.module.fail_json.called)
 
     @patch.object(clc_loadbalancer, 'clc_sdk')
-    @patch.object(ClcLoadBalancer, 'set_clc_credentials_from_env')
+    @patch.object(ClcLoadBalancer, '_set_clc_credentials_from_env')
     def test_process_request_state_nodes_present_no_lb(self,
                                           mock_set_clc_credentials,
                                           mock_clc_sdk):
@@ -144,7 +144,7 @@ class TestClcLoadbalancerFunctions(unittest.TestCase):
         self.assertFalse(self.module.fail_json.called)
 
     @patch.object(clc_loadbalancer, 'clc_sdk')
-    @patch.object(ClcLoadBalancer, 'set_clc_credentials_from_env')
+    @patch.object(ClcLoadBalancer, '_set_clc_credentials_from_env')
     def test_process_request_state_nodes_present_no_pool(self,
                                           mock_set_clc_credentials,
                                           mock_clc_sdk):
@@ -170,7 +170,7 @@ class TestClcLoadbalancerFunctions(unittest.TestCase):
         self.assertFalse(self.module.fail_json.called)
 
     @patch.object(clc_loadbalancer, 'clc_sdk')
-    @patch.object(ClcLoadBalancer, 'set_clc_credentials_from_env')
+    @patch.object(ClcLoadBalancer, '_set_clc_credentials_from_env')
     def test_process_request_state_nodes_absent_no_lb(self,
                                           mock_set_clc_credentials,
                                           mock_clc_sdk):
@@ -196,7 +196,7 @@ class TestClcLoadbalancerFunctions(unittest.TestCase):
         self.assertFalse(self.module.fail_json.called)
 
     @patch.object(clc_loadbalancer, 'clc_sdk')
-    @patch.object(ClcLoadBalancer, 'set_clc_credentials_from_env')
+    @patch.object(ClcLoadBalancer, '_set_clc_credentials_from_env')
     def test_process_request_state_nodes_absent_no_pool(self,
                                           mock_set_clc_credentials,
                                           mock_clc_sdk):
@@ -222,7 +222,7 @@ class TestClcLoadbalancerFunctions(unittest.TestCase):
         self.assertFalse(self.module.fail_json.called)
 
     @patch.object(clc_loadbalancer, 'clc_sdk')
-    @patch.object(ClcLoadBalancer, 'set_clc_credentials_from_env')
+    @patch.object(ClcLoadBalancer, '_set_clc_credentials_from_env')
     def test_process_request_state_nodes_absent(self,
                                           mock_set_clc_credentials,
                                           mock_clc_sdk):
@@ -258,6 +258,7 @@ class TestClcLoadbalancerFunctions(unittest.TestCase):
             'nodes':[{ 'ipAddress': '10.82.152.15', 'privatePort': 82, 'status': 'enabled' }],
             'state': 'nodes_absent'
         }
+        self.module.check_mode = False
         test = ClcLoadBalancer(self.module)
         mock_get_pool_nodes.return_value = [{ 'ipAddress': '10.82.152.15', 'privatePort': 80, 'status': 'enabled' }]
         mock_set_pool_nodes.return_value = 'success'
@@ -277,12 +278,91 @@ class TestClcLoadbalancerFunctions(unittest.TestCase):
             'nodes':[{ 'ipAddress': '10.82.152.15', 'privatePort': 80, 'status': 'enabled' }],
             'state': 'nodes_absent'
         }
+        self.module.check_mode = False
         test = ClcLoadBalancer(self.module)
         mock_get_pool_nodes.return_value = [{ 'ipAddress': '10.82.152.15', 'privatePort': 80, 'status': 'enabled' }]
         mock_set_pool_nodes.return_value = 'success'
         result = test.remove_lbpool_nodes('alias','location','lb_id','pool_id',
                                        [{ 'ipAddress': '10.82.152.15', 'privatePort': 80}])
         self.assertFalse(self.module.fail_json.called)
+        self.assertEqual(result, (True, 'success'))
+
+    @patch.object(clc_loadbalancer, 'clc_sdk')
+    def test_set_loadbalancernodes_pool_id(self, mock_clc_sdk):
+
+        mock_clc_sdk.v2.API.Call.side_effect = [
+            [{'id': 'test', 'name': 'test'}],
+            [{'port': '80', 'id':'test'}],
+            [{ 'ipAddress': '10.82.152.15', 'privatePort': 81, 'status': 'enabled' }]
+        ]
+        self.module.check_mode = False
+        test = ClcLoadBalancer(self.module)
+        result = test.set_loadbalancernodes('alias', 'location', 'lb_id', 'pool_id', [1,2,3])
+        self.assertIsNotNone(result)
+        self.assertEqual(result[0].get('id'), 'test')
+
+    @patch.object(clc_loadbalancer, 'clc_sdk')
+    def test_set_loadbalancernodes_lb_id_none(self, mock_clc_sdk):
+
+        mock_clc_sdk.v2.API.Call.side_effect = [
+            [{'id': 'test', 'name': 'test'}],
+            [{'port': '80', 'id':'test'}],
+            [{ 'ipAddress': '10.82.152.15', 'privatePort': 81, 'status': 'enabled' }]
+        ]
+        self.module.check_mode = False
+        test = ClcLoadBalancer(self.module)
+        result = test.set_loadbalancernodes('alias', 'location', None, 'pool_id', [1,2,3])
+        self.assertIsNone(result)
+
+    @patch.object(clc_loadbalancer, 'clc_sdk')
+    @patch.object(ClcLoadBalancer, '_get_lbpool_nodes')
+    def test_loadbalancerpool_nodes_exists_true(self, mock_get_pool_nodes, mock_clc_sdk):
+        #Setup
+        self.module.params = {
+            'name': 'test',
+            'port': 80,
+            'nodes':[{ 'ipAddress': '10.82.152.15', 'privatePort': 80, 'status': 'enabled' }],
+            'state': 'nodes_absent'
+        }
+        self.module.check_mode = False
+        test = ClcLoadBalancer(self.module)
+        mock_get_pool_nodes.return_value = [{ 'ipAddress': '10.82.152.15', 'privatePort': 80, 'status': 'enabled' }]
+        result = test._loadbalancerpool_nodes_exists('alias','location',80,'lb_id','pool_id',
+                                       [{ 'ipAddress': '10.82.152.15', 'privatePort': 80}])
+        self.assertEqual(result, True)
+
+    @patch.object(clc_loadbalancer, 'clc_sdk')
+    @patch.object(ClcLoadBalancer, '_get_lbpool_nodes')
+    def test_loadbalancerpool_nodes_exists_false(self, mock_get_pool_nodes, mock_clc_sdk):
+        #Setup
+        self.module.params = {
+            'name': 'test',
+            'port': 80,
+            'nodes':[{ 'ipAddress': '10.82.152.15', 'privatePort': 80, 'status': 'enabled' }],
+            'state': 'nodes_absent'
+        }
+        self.module.check_mode = False
+        test = ClcLoadBalancer(self.module)
+        mock_get_pool_nodes.return_value = [{ 'ipAddress': '10.82.152.15', 'privatePort': 80, 'status': 'enabled' }]
+        result = test._loadbalancerpool_nodes_exists('alias','location',80,'lb_id','pool_id',
+                                       [{ 'ipAddress': '10.82.152.15', 'privatePort': 90}])
+        self.assertEqual(result, False)
+
+    @patch.object(clc_loadbalancer, 'clc_sdk')
+    @patch.object(ClcLoadBalancer, '_loadbalancer_exists')
+    @patch.object(ClcLoadBalancer, '_get_loadbalancer_id')
+    @patch.object(ClcLoadBalancer, '_loadbalancerpool_exists')
+    @patch.object(ClcLoadBalancer, '_loadbalancerpool_nodes_exists')
+    @patch.object(ClcLoadBalancer, 'set_loadbalancernodes')
+    def test_ensure_lbpool_nodes_set(self, set_lb_nodes, lbpool_nodes_exists, lbpool_exists, get_lb_id, lb_exists, mock_clc_sdk):
+        lb_exists.return_value = True
+        get_lb_id.return_value = '123'
+        lbpool_exists.retrun_value = True
+        lbpool_nodes_exists.return_value = False
+        set_lb_nodes.return_value = 'success'
+        self.module.check_mode = False
+        test = ClcLoadBalancer(self.module)
+        result = test.ensure_lbpool_nodes_set('alias', 'location', 'name', 80, [1,2,3])
         self.assertEqual(result, (True, 'success'))
 
     def test_clc_module_not_found(self):
@@ -302,19 +382,28 @@ class TestClcLoadbalancerFunctions(unittest.TestCase):
         # Reset clc_group
         reload(clc_loadbalancer)
 
-    def test_clc_set_credentials_w_creds(self):
-        with patch.dict('os.environ', {'CLC_V2_API_USERNAME': 'hansolo', 'CLC_V2_API_PASSWD': 'falcon'}):
-            with patch.object(clc_loadbalancer, 'clc_sdk') as mock_clc_sdk:
-                under_test = ClcLoadBalancer(self.module)
-                under_test.set_clc_credentials_from_env()
+    @patch.object(ClcLoadBalancer, 'clc')
+    def test_set_clc_credentials_from_env(self, mock_clc_sdk):
+        with patch.dict('os.environ', {'CLC_V2_API_TOKEN': 'dummyToken',
+                                       'CLC_ACCT_ALIAS': 'TEST'}):
+            under_test = ClcLoadBalancer(self.module)
+            under_test._set_clc_credentials_from_env()
+        self.assertEqual(under_test.clc._LOGIN_TOKEN_V2, 'dummyToken')
+        self.assertFalse(mock_clc_sdk.v2.SetCredentials.called)
+        self.assertEqual(self.module.fail_json.called, False)
 
-        mock_clc_sdk.v2.SetCredentials.assert_called_once_with(api_username='hansolo', api_passwd='falcon')
+    @patch.object(ClcLoadBalancer, 'clc')
+    def test_set_clc_credentials_w_api_url(self, mock_clc_sdk):
+        with patch.dict('os.environ', {'CLC_V2_API_URL': 'dummyapiurl'}):
+            under_test = ClcLoadBalancer(self.module)
+            under_test._set_clc_credentials_from_env()
+            self.assertEqual(under_test.clc.defaults.ENDPOINT_URL_V2, 'dummyapiurl')
 
 
     def test_clc_set_credentials_w_no_creds(self):
         with patch.dict('os.environ', {}, clear=True):
             under_test = ClcLoadBalancer(self.module)
-            under_test.set_clc_credentials_from_env()
+            under_test._set_clc_credentials_from_env()
 
         self.assertEqual(self.module.fail_json.called, True)
 
