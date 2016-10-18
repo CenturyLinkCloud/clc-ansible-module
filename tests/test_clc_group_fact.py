81b4fe54a205edb3b3f3be48ac6c19d5418b7b50
Benjamin R. J. Schwedler <benjamin.schwedler@ctl.io>
2016-10-18 08:23:59 -0500

    Add basic tests for clc_group_fact
      - Test for presence of requests module
      - Test for argument_spec
      - Test for correct group endpoint
      - 2/3 of test for setting credentials from env

--- tests/test_clc_group_fact.py
+++ tests/test_clc_group_fact.py
@@ -24,3 +24,73 @@ import mock
 from mock import patch
 import unittest
 
+class TestClcGroupFactFunctions(unittest.TestCase):
+
+    def setUp(self):
+        self.clc = mock.MagicMock()
+        self.module = mock.MagicMock()
+        self.datacenter = mock.MagicMock()
+
+    def test_requests_module_not_found(self):
+        # Setup Mock Import Function
+        real_import = __import__
+
+        def mock_import(name, *args):
+            if name == 'requests':
+                args[0]['requests'].__version__ = '2.7.0'
+                raise ImportError
+            return real_import(name, *args)
+        # Under Test
+        with mock.patch('__builtin__.__import__', side_effect=mock_import):
+            reload(clc_group_fact)
+            ClcGroupFact(self.module)
+        # Assert Expected Behavior
+        self.module.fail_json.assert_called_with(
+            msg='requests library is required for this module')
+
+        # Reset
+        reload(clc_group_fact)
+
+    def test_process_request(self):
+        pass
+
+    def test_define_argument_spec(self):
+        result = ClcGroupFact._define_module_argument_spec()
+        self.assertIsInstance(result, dict)
+        self.assertTrue('argument_spec' in result)
+        self.assertEqual(
+            result['argument_spec'],
+            {'group_id': {'required': True}})
+
+    def test_get_endpoint(self):
+        under_test = ClcGroupFact(self.module)
+        under_test.api_url = 'http://unittest.example.com'
+        under_test.clc_alias = 'test_alias'
+        self.assertEqual(
+            under_test._get_endpoint('test_group'),
+            'http://unittest.example.com/v2/groups/test_alias/test_group')
+
+    def test_set_clc_credentials_from_env(self):
+        # Required combination of credentials not passed
+        with patch.dict('os.environ', {
+                'CLC_V2_API_URL': 'http://unittest.example.com', }):
+            under_test = ClcGroupFact(self.module)
+            under_test._set_clc_credentials_from_env()
+        self.module.fail_json.assert_called_with(
+            msg='You must set the CLC_V2_API_USERNAME and CLC_V2_API_PASSWD '
+                'environment variables')
+        # Token and alias
+        with patch.dict('os.environ', {
+                'CLC_V2_API_URL': 'http://unittest.example.com',
+                'CLC_V2_API_TOKEN': 'dummy_token',
+                'CLC_ACCT_ALIAS': 'dummy_alias'}):
+            under_test = ClcGroupFact(self.module)
+            under_test._set_clc_credentials_from_env()
+        self.assertEqual(under_test.v2_api_token, 'dummy_token')
+        self.assertEqual(under_test.clc_alias, 'dummy_alias')
+        # Username and password
+        # TODO: Mock requests response from endpoint
+
+
+if __name__ == '__main__':
+    unittest.main()
