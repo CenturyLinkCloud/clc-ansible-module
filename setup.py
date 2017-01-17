af1e1b58b72e48f6efbbf3498d9212cfdd8e3b48
markramach <mark.ramach@centurylink.com>
2017-01-17 10:27:36 -0600

    new version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except ImportError:
 
 setup(
     name='clc-ansible-module',
-    version='1.1.17',
+    version='1.1.18',
     description='Centurylink Cloud Ansible Modules',
     author='CenturyLink Cloud',
     author_email='WFAAS-LLFT@centurylink.com',
