52bde865c57edd7c288c082044b37174087727e4
Justin Colbert <jwcolbert@users.noreply.github.com>
2017-08-16 13:57:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ except ImportError:
 
 setup(
     name='clc-ansible-module',
-    version='1.1.22',
+    version='1.1.23',
     description='Centurylink Cloud Ansible Modules',
     author='CenturyLink Cloud',
     author_email='WFAAS-LLFT@centurylink.com',
